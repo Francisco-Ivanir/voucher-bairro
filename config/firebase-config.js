508ee73dfd94c1914e498d55f8ca745c3114657f
Francisco-Ivanir <ficfivanir@outlook.com>
2026-08-27 16:04:27 -0300

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAdy7i6Lk2q8sFnDOL0nij9iABVJSsrqiw",
+  authDomain: "voucher-bairro.firebaseapp.com",
+  projectId: "voucher-bairro",
+  storageBucket: "voucher-bairro.firebasestorage.app",
+  messagingSenderId: "657154974832",
+  appId: "1:657154974832:web:58846f0bc552464edb40b5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
