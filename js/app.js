afbda95c81fa5cb8e2410d277f2f3ecacfb315a9
Francisco-Ivanir <ficfivanir@outlook.com>
2026-08-27 21:31:45 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 import { app } from "../config/firebase-config.js";
 
-console.log("Firebase conectado:", app);
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+
+
+const db = getFirestore(app);
+
+
+async function carregarVouchers(){
+
+  const consulta = await getDocs(collection(db, "vouchers"));
+
+  consulta.forEach((doc) => {
+
+    console.log("Voucher encontrado:", doc.id, doc.data());
+
+  });
+
+}
+
+
+carregarVouchers();
