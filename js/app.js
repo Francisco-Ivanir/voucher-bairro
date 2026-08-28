aa23bbfe6a7eac207f04aa402ef337bf4395f201
Francisco-Ivanir <ficfivanir@outlook.com>
2026-08-27 22:33:13 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,46 +3,80 @@ import { app } from "../config/firebase-config.js";
 import {
   getFirestore,
   collection,
-  getDocs
+  getDocs,
+  query,
+  where
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
 
 
 const db = getFirestore(app);
 
 
-async function carregarVouchers(){
-
-  const area = document.getElementById("voucher");
-
-  const consulta = await getDocs(collection(db, "vouchers"));
+const botao = document.getElementById("btnConsultar");
 
 
-  consulta.forEach((doc) => {
+botao.addEventListener("click", async () => {
+
+
+  const codigoDigitado =
+    document.getElementById("codigoVoucher").value;
+
+
+  const resultado =
+    document.getElementById("resultado");
+
+
+  resultado.innerHTML = "Consultando...";
+
+
+  const consulta = query(
+    collection(db, "vouchers"),
+    where("codigo", "==", codigoDigitado)
+  );
+
+
+  const resposta = await getDocs(consulta);
+
+
+  if(resposta.empty){
+
+    resultado.innerHTML =
+    "❌ Voucher não encontrado.";
+
+    return;
+
+  }
+
+
+  resposta.forEach((doc) => {
+
 
     const dados = doc.data();
 
 
-    area.innerHTML = `
+    resultado.innerHTML = `
 
-    <h2>🎟️ Benefício encontrado</h2>
+    <h3>🎟️ Voucher encontrado</h3>
 
     <p>
+    Benefício:
     ${dados.beneficio}
     </p>
 
     <p>
-    Status: ${dados.status}
+    Status:
+    ${dados.status}
     </p>
 
-<p>
-Código do voucher: ${dados.codigo}
-</p>
+    <p>
+    Código:
+    ${dados.codigo}
+    </p>
 
     `;
 
+
   });
 
-}
 
-
-carregarVouchers();
+});
