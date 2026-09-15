1f41050bf04c3043403f93618b996a04c155f928
Francisco-Ivanir <ficfivanir@outlook.com>
2026-09-14 22:15:03 -0300

    Update lojista.js

--- js/lojista.js
+++ js/lojista.js
@@ -556,34 +556,8 @@ respostaLoja.forEach((documentoLoja) => {
   const dadosLoja =
     documentoLoja.data();
 
-  let statusVoucher = "ativo";
-
-if (
-  dadosLoja.usado === true ||
-  dadosLoja.status === "utilizado"
-) {
-
-  statusVoucher = "utilizado";
-
-} else {
-
-  const partesFiltro =
-    dadosLoja.dataValidade.split("/");
-
-  const validadeFiltro =
-    new Date(
-      partesFiltro[2],
-      partesFiltro[1] - 1,
-      partesFiltro[0]
-    );
-
-  if (new Date() > validadeFiltro) {
-
-    statusVoucher = "vencido";
-
-  }
-
-}
+ const statusVoucher =
+  obterStatusVoucher(dadosLoja);
 
   if (
   filtroStatus !== "todos" &&
